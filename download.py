39168043141915630cf220e3c193adc6cd6b6935
Saurav Poudel <sawrov@hotmail.com>
2020-12-01 11:06:39 +0800

    Update download.py

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ import subprocess
 f=open("links",'r')
 while f.readline():
   link=f.readline()
-  downloadlink="https://www.arimgsas.com.au/"+(ul.unquote(link))
+  downloadlink="https://www.---.com.au/"+(ul.unquote(link))
   bash = "wget "+downloadlink
   process=subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
   _,_=process.communicate()
