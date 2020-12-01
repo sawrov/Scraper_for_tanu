405b8d1f511a15eb3072e33f861540f8e07ca861
Saurav Poudel <sawrov@hotmail.com>
2020-12-01 11:07:42 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ echo "EXTRACTING PDF LINKS FROM THE LECTURES"
 echo "CHECK raw_links.txt TO SEE UPDATES OF LINKS"
 echo "WILL TAKE A WHILE"
 sudo cat August_sesion.txt | grep https | xargs curl -s -H "$(cat wget_cookie.txt)" | grep \.pdf > raw_links.txt
-cat raw_links.txt | grep -Eo "wp-content.*\.pdf" > encoded_links
+cat raw_links.txt | grep -Eo "wp-content.*\.pdf" >links
 python3 download.py
